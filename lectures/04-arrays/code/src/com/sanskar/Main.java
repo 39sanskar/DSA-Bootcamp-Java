1cf3976f6abbb7099dc160ce62d17546aca1aa78
39sanskar <39sanskar@gmail.com>
2025-05-24 22:10:20 +0530

    Program

--- lectures/04-arrays/code/src/com/sanskar/Main.java
+++ lectures/04-arrays/code/src/com/sanskar/Main.java
@@ -4,15 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         // Q: store a roll number
+        /* 
         int  a = 19;
-
+        */
         // Q: store a persons name
+        /* 
         String name = "Sanskar Mishra";
 
         // Q: store 5 roll numbers
         int roll1 = 23;
         int roll2 = 55;
         int roll3 = 18;
+
+        */
 
         // syntax
         // datatype[] variable_name = new datatype[size];
@@ -21,10 +25,14 @@ public class Main {
 //        int[] rnos = new int[5];
 //        // or directly
 //        int[] rnos2 = {23, 12, 45, 32, 15};
+        
+        /* 
 
         int[] ros;  // declaration of array. ros is getting defined in the stack
         ros = new int[5]; // initialisation actually here object is being created in the memory (heap)
 
+        */
+ 
 
 //        System.out.println(ros[1]);
 
@@ -40,6 +48,6 @@ public class Main {
 //            System.out.println(element);
 //        }
 
-
+      
     }
 }
